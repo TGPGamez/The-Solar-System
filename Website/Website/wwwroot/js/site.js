0475b37f072a76c9c9402a6cb656dff1e9de8419
KoLorca <100674270+Lorinc-Vagi@users.noreply.github.com>
2022-03-14 14:31:18 +0100

    pain

--- Website/Website/wwwroot/js/site.js
+++ Website/Website/wwwroot/js/site.js
@@ -2,3 +2,235 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+"use strict";
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+/*
+ window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+    if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+} 
+
+
+*/
+/* on1-8() and off() is similar */
+function on1() {
+    document.getElementById("window").style.display = "block";
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("imgplanet1").style.display = "block";
+    document.getElementById("imgplanet2").style.display = "none";
+    document.getElementById("imgplanet3").style.display = "none";
+    document.getElementById("imgplanet4").style.display = "none";
+    document.getElementById("imgplanet5").style.display = "none";
+    document.getElementById("imgplanet6").style.display = "none";
+    document.getElementById("imgplanet7").style.display = "none";
+    document.getElementById("imgplanet8").style.display = "none";
+
+    // from here the "txtplanets"
+    document.getElementById("txtplanet1").style.display = "block";
+    document.getElementById("txtplanet2").style.display = "none";
+    document.getElementById("txtplanet3").style.display = "none";
+    document.getElementById("txtplanet4").style.display = "none";
+    document.getElementById("txtplanet5").style.display = "none";
+    document.getElementById("txtplanet6").style.display = "none";
+    document.getElementById("txtplanet7").style.display = "none";
+    document.getElementById("txtplanet8").style.display = "none";
+}
+function on2() {
+    document.getElementById("window").style.display = "block";
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("imgplanet1").style.display = "none";
+    document.getElementById("imgplanet2").style.display = "block";
+    document.getElementById("imgplanet3").style.display = "none";
+    document.getElementById("imgplanet4").style.display = "none";
+    document.getElementById("imgplanet5").style.display = "none";
+    document.getElementById("imgplanet6").style.display = "none";
+    document.getElementById("imgplanet7").style.display = "none";
+    document.getElementById("imgplanet8").style.display = "none";
+    // from here the "txtplanets"
+    document.getElementById("txtplanet1").style.display = "none";
+    document.getElementById("txtplanet2").style.display = "block";
+    document.getElementById("txtplanet3").style.display = "none";
+    document.getElementById("txtplanet4").style.display = "none";
+    document.getElementById("txtplanet5").style.display = "none";
+    document.getElementById("txtplanet6").style.display = "none";
+    document.getElementById("txtplanet7").style.display = "none";
+    document.getElementById("txtplanet8").style.display = "none";
+}
+function on3() {
+    document.getElementById("window").style.display = "block";
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("imgplanet1").style.display = "none";
+    document.getElementById("imgplanet2").style.display = "none";
+    document.getElementById("imgplanet3").style.display = "block";
+    document.getElementById("imgplanet4").style.display = "none";
+    document.getElementById("imgplanet5").style.display = "none";
+    document.getElementById("imgplanet6").style.display = "none";
+    document.getElementById("imgplanet7").style.display = "none";
+    document.getElementById("imgplanet8").style.display = "none";
+    // from here the "txtplanets"
+    document.getElementById("txtplanet1").style.display = "none";
+    document.getElementById("txtplanet2").style.display = "none";
+    document.getElementById("txtplanet3").style.display = "block";
+    document.getElementById("txtplanet4").style.display = "none";
+    document.getElementById("txtplanet5").style.display = "none";
+    document.getElementById("txtplanet6").style.display = "none";
+    document.getElementById("txtplanet7").style.display = "none";
+    document.getElementById("txtplanet8").style.display = "none";
+}
+function on4() {
+    document.getElementById("window").style.display = "block";
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("imgplanet1").style.display = "none";
+    document.getElementById("imgplanet2").style.display = "none";
+    document.getElementById("imgplanet3").style.display = "none";
+    document.getElementById("imgplanet4").style.display = "block";
+    document.getElementById("imgplanet5").style.display = "none";
+    document.getElementById("imgplanet6").style.display = "none";
+    document.getElementById("imgplanet7").style.display = "none";
+    document.getElementById("imgplanet8").style.display = "none";
+    // from here the "txtplanets"
+    document.getElementById("txtplanet1").style.display = "none";
+    document.getElementById("txtplanet2").style.display = "none";
+    document.getElementById("txtplanet3").style.display = "none";
+    document.getElementById("txtplanet4").style.display = "block";
+    document.getElementById("txtplanet5").style.display = "none";
+    document.getElementById("txtplanet6").style.display = "none";
+    document.getElementById("txtplanet7").style.display = "none";
+    document.getElementById("txtplanet8").style.display = "none";
+}
+function on5() {
+    document.getElementById("window").style.display = "block";
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("imgplanet1").style.display = "none";
+    document.getElementById("imgplanet2").style.display = "none";
+    document.getElementById("imgplanet3").style.display = "none";
+    document.getElementById("imgplanet4").style.display = "none";
+    document.getElementById("imgplanet5").style.display = "block";
+    document.getElementById("imgplanet6").style.display = "none";
+    document.getElementById("imgplanet7").style.display = "none";
+    document.getElementById("imgplanet8").style.display = "none";
+    // from here the "txtplanets"
+    document.getElementById("txtplanet1").style.display = "none";
+    document.getElementById("txtplanet2").style.display = "none";
+    document.getElementById("txtplanet3").style.display = "none";
+    document.getElementById("txtplanet4").style.display = "none";
+    document.getElementById("txtplanet5").style.display = "block";
+    document.getElementById("txtplanet6").style.display = "none";
+    document.getElementById("txtplanet7").style.display = "none";
+    document.getElementById("txtplanet8").style.display = "none";
+}
+function on6() {
+    document.getElementById("window").style.display = "block";
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("imgplanet1").style.display = "none";
+    document.getElementById("imgplanet2").style.display = "none";
+    document.getElementById("imgplanet3").style.display = "none";
+    document.getElementById("imgplanet4").style.display = "none";
+    document.getElementById("imgplanet5").style.display = "none";
+    document.getElementById("imgplanet6").style.display = "block";
+    document.getElementById("imgplanet7").style.display = "none";
+    document.getElementById("imgplanet8").style.display = "none";
+    // from here the "txtplanets"
+    document.getElementById("txtplanet1").style.display = "none";
+    document.getElementById("txtplanet2").style.display = "none";
+    document.getElementById("txtplanet3").style.display = "none";
+    document.getElementById("txtplanet4").style.display = "none";
+    document.getElementById("txtplanet5").style.display = "none";
+    document.getElementById("txtplanet6").style.display = "block";
+    document.getElementById("txtplanet7").style.display = "none";
+    document.getElementById("txtplanet8").style.display = "none";
+}
+function on7() {
+    document.getElementById("window").style.display = "block";
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("imgplanet1").style.display = "none";
+    document.getElementById("imgplanet2").style.display = "none";
+    document.getElementById("imgplanet3").style.display = "none";
+    document.getElementById("imgplanet4").style.display = "none";
+    document.getElementById("imgplanet5").style.display = "none";
+    document.getElementById("imgplanet6").style.display = "none";
+    document.getElementById("imgplanet7").style.display = "block";
+    document.getElementById("imgplanet8").style.display = "none";
+    // from here the "txtplanets"
+    document.getElementById("txtplanet1").style.display = "none";
+    document.getElementById("txtplanet2").style.display = "none";
+    document.getElementById("txtplanet3").style.display = "none";
+    document.getElementById("txtplanet4").style.display = "none";
+    document.getElementById("txtplanet5").style.display = "none";
+    document.getElementById("txtplanet6").style.display = "none";
+    document.getElementById("txtplanet7").style.display = "block";
+    document.getElementById("txtplanet8").style.display = "none";
+}
+function on8() {
+    document.getElementById("window").style.display = "block";
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("imgplanet1").style.display = "none";
+    document.getElementById("imgplanet2").style.display = "none";
+    document.getElementById("imgplanet3").style.display = "none";
+    document.getElementById("imgplanet4").style.display = "none";
+    document.getElementById("imgplanet5").style.display = "none";
+    document.getElementById("imgplanet6").style.display = "none";
+    document.getElementById("imgplanet7").style.display = "none";
+    document.getElementById("imgplanet8").style.display = "block";
+    // from here the "txtplanets"
+    document.getElementById("txtplanet1").style.display = "none";
+    document.getElementById("txtplanet2").style.display = "none";
+    document.getElementById("txtplanet3").style.display = "none";
+    document.getElementById("txtplanet4").style.display = "none";
+    document.getElementById("txtplanet5").style.display = "none";
+    document.getElementById("txtplanet6").style.display = "none";
+    document.getElementById("txtplanet7").style.display = "none";
+    document.getElementById("txtplanet8").style.display = "block";
+}
+function off() {
+    document.getElementById("window").style.display = "none";
+    document.getElementById("overlay").style.display = "none";
+    document.getElementById("imgplanet1").style.display = "none";
+    document.getElementById("imgplanet2").style.display = "none";
+    document.getElementById("imgplanet3").style.display = "none";
+    document.getElementById("imgplanet4").style.display = "none";
+    document.getElementById("imgplanet5").style.display = "none";
+    document.getElementById("imgplanet6").style.display = "none";
+    document.getElementById("imgplanet7").style.display = "none";
+    document.getElementById("imgplanet8").style.display = "none";
+    // from here the "txtplanets"
+    document.getElementById("txtplanet1").style.display = "none";
+    document.getElementById("txtplanet2").style.display = "none";
+    document.getElementById("txtplanet3").style.display = "none";
+    document.getElementById("txtplanet4").style.display = "none";
+    document.getElementById("txtplanet5").style.display = "none";
+    document.getElementById("txtplanet6").style.display = "none";
+    document.getElementById("txtplanet7").style.display = "none";
+    document.getElementById("txtplanet8").style.display = "none";
+}
+
+function language() { }
+
+function myFunction() {
+    var x = document.getElementById("myNavbar");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
+
+function on() {
+    document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+    document.getElementById("overlay").style.display = "none";
+}
