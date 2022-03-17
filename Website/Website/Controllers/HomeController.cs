2fec4384e20b936a73447fe830fc34d33460b738
TGPGamez <58465104+TGPGamez@users.noreply.github.com>
2022-03-17 19:29:47 +0100

    gg

--- Website/Website/Controllers/HomeController.cs
+++ Website/Website/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using Website.Models;
 using Website.Services;
 
@@ -35,5 +38,32 @@ namespace Website.Controllers
 
             return LocalRedirect(returnUrl);
         }
+
+        public void Sender(string planet)
+        {
+
+            IPAddress ip = IPAddress.Parse("10.108.233.59");
+            int port = 5000;
+            TcpClient client = new TcpClient();
+            client.Connect(ip, port);
+            Console.WriteLine("Client Connected");
+            NetworkStream ns = client.GetStream();
+            Console.WriteLine(planet);
+            byte[] buffer = Encoding.ASCII.GetBytes(planet);
+            try
+            {
+                ns.Write(buffer, 0, buffer.Length);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("FUCK");
+            }
+            client.Client.Shutdown(SocketShutdown.Send);
+            ns.Close();
+            client.Close();
+            Console.WriteLine("Disconnect from server");
+            
+        }
+
     }
 }
